00af6f3d04375ded38ec955b09f8ae7d23b48ab7
Diorelyon Amador <diorel_x@hotmail.com>
2016-10-02 17:13:01 -0500

    cierre de sesion
    se sierra la secion pero no funciona bien el control

---
+++ CandidatosSistema/CandidatosSistema/Controllers/CerrarSesionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CandidatosSistema.Controllers
+{
+    public class CerrarSesionController : Controller
+    {
+        // GET: CerrarSesion
+        public ActionResult Index()
+        {
+
+            Session.Abandon();
+            Response.Cookies.Add(new HttpCookie("ASP.NET_SessionId", ""));
+
+
+            return View();
+        }
+    }
+}
