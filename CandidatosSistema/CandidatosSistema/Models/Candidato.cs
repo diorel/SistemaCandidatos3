3ee26d9cfcc4dcf57e3019a2ba4897072c5c0df1
Diorelyon Amador <diorel_x@hotmail.com>
2016-11-18 10:06:02 -0600

    modelo vacantes
    se agregaron los modelos de vacantes  con sus catalogos

--- CandidatosSistema/CandidatosSistema/Models/Candidato.cs
+++ CandidatosSistema/CandidatosSistema/Models/Candidato.cs
@@ -13,60 +13,44 @@ namespace CandidatosSistema.Models
 {
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-
-
-    public partial class Candidato
+    using System.Collections.Generic;
+    
+public partial class Candidato
 {
 
     public int CandidatoId { get; set; }
-    [Required(ErrorMessage = "El Nombre es requerido")]
-    [Display(Name = "Nombre:", Description = "xxxxx")]
+
     public string Nombre { get; set; }
-    [Required(ErrorMessage = "El Telefono es Requerido")]
-    [Range(0, 999999999999999999, ErrorMessage = "La edad debe estar entre los 12 y los 50 años")]
-    [Display(Name = "Telefono:", Description = "xxxxx")]
-     public string Telefono { get; set; }
-     [DataType(DataType.EmailAddress, ErrorMessage = "E-mail no es valido")]
-     [Required(ErrorMessage = "El Correo es Requerido")]
-     [Display(Name = "Email:", Description = "xxxxx")]
-     public string Correo { get; set; }
-     [Required(ErrorMessage = "Elija una Entidad")]
-     [Display(Name = "Estado", Description = "xxxxx")]
-     public Nullable<int> LocalidadId { get; set; }
-     [Required(ErrorMessage = "Elija un Salario")]
-     public Nullable<int> SueldoId { get; set; }
-     [Required(ErrorMessage = "Elija un garado de Estudios")]
-      public Nullable<int> EscolaridadId { get; set; }
-     [Required(ErrorMessage = "Elija una especialidad")]
-     public Nullable<int> EspecialidadId { get; set; }
 
-     public Nullable<bool> EstadoCandidato { get; set; }
-     [Display(Name = "Capturista:", Description = "xxxxx")]
-     public string Capturista { get; set; }
-     [Display(Name = "Fecha de Captura:", Description = "xxxxx")]
-     public Nullable<System.DateTime> FechaCaptura { get; set; }
-     [Display(Name = "CV:", Description = "xxxxx")]
-     [Required(ErrorMessage = "selecione un archivo")]
+    public string Telefono { get; set; }
 
-     public string Archivo { get; set; }
-     [Required(ErrorMessage = "Es requerido este dato")]
-     [Display(Name = "Delegacion/Municipio", Description = "xxxxx")]
+    public string Correo { get; set; }
 
-     public string Municipio_colonia { get; set; }
+    public Nullable<int> LocalidadId { get; set; }
+
+    public Nullable<int> SueldoId { get; set; }
+
+    public Nullable<int> EscolaridadId { get; set; }
+
+    public Nullable<int> EspecialidadId { get; set; }
+
+    public Nullable<bool> EstadoCandidato { get; set; }
+
+    public string Capturista { get; set; }
+
+    public Nullable<System.DateTime> FechaCaptura { get; set; }
+
+    public string Archivo { get; set; }
+
+    public string Municipio_colonia { get; set; }
 
     public Nullable<int> EstatusId { get; set; }
-    [Display(Name = "Comentario Estatus:", Description = "xxxxx")]
 
     public string ComentarioEstatus { get; set; }
-    [Display(Name = "Subespecialidad:", Description = "xxxxx")]
 
     public string Area { get; set; }
-  
-     public Nullable<int> EmpresaId { get; set; }
 
-
+    public Nullable<int> EmpresaId { get; set; }
 
 
 
